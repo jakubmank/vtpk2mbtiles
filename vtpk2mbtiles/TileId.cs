06cc61383ee3d3d60c0f72950230b21a51291125
Jakub Mank <mank@cartegraph.com>
2022-12-09 19:45:12 +0100

    Add constructor to TiedId class for easier testing purposes.

--- vtpk2mbtiles/TileId.cs
+++ vtpk2mbtiles/TileId.cs
@@ -9,8 +9,20 @@ namespace vtpk2mbtiles {
 		public long y { get; set; }
 
 		public long TmsY { get { return ((1 << z) - y - 1); } }
+		public TileId()
+		{
 
-		public override string? ToString() {
+		}
+		public TileId(int z, int x, int y)
+		{
+			// init class members with params
+			this.z = z;
+			this.x = x;
+			this.y = y;
+		}
+
+        public override string? ToString()
+        {
 			return $"z:{z} row/y:{y} col/x:{x}";
 		}
 	}
